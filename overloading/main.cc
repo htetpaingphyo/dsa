8f6c83c20657b27f8a70008afb43c7985ef742e1
htetpaingphyo <mr.htetpaing.mm@gmail.com>
2024-08-11 19:38:26 +0700

    operator overloading

--- overloading/main.cc
+++ overloading/main.cc
@@ -6,9 +6,15 @@ private:
     int num;
 
 public:
-    Foo(int num) : num(num) {}              // initializer : equal to constructor assign to private variable
-    friend int operator+(Foo &f1, Foo &f2); // using friend operator to overload operator and access private method
+    Foo(int num) : num(num) {}                                       // initializer : equal to constructor assign to private variable
+    friend int operator+(Foo &f1, Foo &f2);                          // using friend operator to overload operator and access private method
+    friend std::ostream &operator<<(std::ostream &os, const Foo &f); // here' we use const for data security
 };
+
+std::ostream &operator<<(std::ostream &os, const Foo &f)
+{
+    return os << f.num;
+}
 
 int operator+(Foo &f1, Foo &f2)
 {
@@ -22,10 +28,12 @@ int main()
     std::cout << "enter num for foo1: ";
     std::cin >> num;
     Foo foo1 = Foo(num);
+    std::cout << "foo1: " << foo1 << std::endl;
 
     std::cout << "enter num for foo2: ";
     std::cin >> num;
     Foo foo2 = Foo(num);
+    std::cout << "foo2: " << foo2 << std::endl;
 
     std::cout << "result of foo1::class + foo2::class => " << foo1 + foo2 << std::endl;
 
