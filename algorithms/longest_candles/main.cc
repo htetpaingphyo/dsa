13e0de9518e895939e7720297578ff8c10adb8da
htetpaingphyo <mr.htetpaing.mm@gmail.com>
2024-08-19 18:06:33 +0700

    add algorithms_longest_candles

---
+++ algorithms/longest_candles/main.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+int birthdayCakeCandles(std::vector<int> candles)
+{
+    int max = -1;
+    int count = 0;
+
+    for (int i = 0; i < candles.size(); i++)
+    {
+        if (candles[i] > max)
+            max = candles[i];
+    }
+
+    for (int i : candles)
+    {
+        if (i == max)
+            count++;
+    }
+
+    return count;
+}
+
+int main()
+{
+    std::vector<int> arr = {9, 2, 14, 7, 11, 14, 3, 5, 8};
+    int result = birthdayCakeCandles(arr);
+    std::cout << result << std::endl;
+    return 0;
+}
