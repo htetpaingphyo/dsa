31470f22abe9d2d3d64b43e08aff563dcc9213a8
htetpaingphyo <mr.htetpaing.mm@gmail.com>
2025-03-06 01:05:24 +0700

    sorting bubble_sort

--- sorting/bubble/main.cc
+++ sorting/bubble/main.cc
@@ -5,13 +5,9 @@ void bubbleSort(std::vector<int> &arr)
 {
     int n = arr.size();
     for (int i = 0; i < n - 1; i++)
-    {
         for (int j = 0; j < n - 1; j++)
-        {
             if (arr[j] > arr[j + 1])
                 std::swap(arr[j], arr[j + 1]);
-        }
-    }
 }
 
 int main()
